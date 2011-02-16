9c17e788e684d1f67c91d05bcfe791f9d57ab199
markrendle <mark@markrendle.net>
2011-02-16 17:24:47 +0000

    Initial commit

---
+++ Simple.Test/BufferedEnumerableTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace Simple.Test
+{
+    public class BufferedEnumerableTest
+    {
+        [Fact(DisplayName = "Cleanup method should be called")]
+        public void ShouldCallCleanup()
+        {
+            // Arrange
+            bool cleanedUp = false;
+
+            var list = BufferedEnumerable.Create("ABC".ToIterator(),() => cleanedUp = true)
+                    .ToList();
+
+            // Act
+            Assert.Equal(3, list.Count);
+
+            SpinWait.SpinUntil(() => cleanedUp, 1000);
+
+            Assert.True(cleanedUp);
+        }
+    }
+}
---
+++ Simple/EnumerableEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple
+{
+    public static class EnumerableEx
+    {
+        public static Func<Tuple<bool,T>> ToIterator<T>(this IEnumerable<T> source)
+        {
+            var enumerator = source.GetEnumerator();
+            return
+                () => enumerator.MoveNext() ? Tuple.Create(true, enumerator.Current) : Tuple.Create(false, default(T));
+        }
+    }
+}
